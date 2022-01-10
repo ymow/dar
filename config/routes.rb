36636be0a35b8403aae47c991714190833ff646e
Ymow Wu <kenshin0812@hotmail.com>
2022-01-10 16:58:26 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :comments
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
